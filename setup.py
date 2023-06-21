252724c218255c2b8c468f82534f184f3ef69988
DingShinn <dingyubo@dp.tech>
2023-06-21 23:55:05 +0800

    update: 更新 manifest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='streamlit-phone-number',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['streamlit'],
